b31a65cf7de33510cf385aef3e4239a1a919da55
Timothy Le <Timothy.Thanh.Le@gmail.com>
2026-04-09 11:51:22 -0700

    created visible border for listing and for explanation

--- src/components/ListingCard.tsx
+++ src/components/ListingCard.tsx
@@ -1,27 +1,35 @@
 function ListingCard({ listing, rank, onExplainMatch, explanation, isLoading }: any) {
     return(
-        <div>
-            <h2>#{rank} - {listing.name}</h2>
-            {listing.matchScore !== undefined && (
-                <p>Match Score: {listing.matchScore}</p>
-            )}
-            <p>Price: ${listing.price}</p>
-            <p>Location: {listing.location}</p>
-            <p>Commute Time: {listing.commuteTime} minutes</p>
-            <p>Lease Term: {listing.leaseTerm} months</p>
-            <p>Number of Bedrooms: {listing.numBedroom}</p>
-            <p>{listing.furnished && "Furnished"}</p>
-            <p>{listing.laundry && "Laundry"}</p>
-            <p>{listing.parking && "Parking"}</p>
-            <button onClick={() => onExplainMatch(listing)}>
-                {explanation ? "Close Explanation" : "Review Listing"}
-            </button>
-            {isLoading && <p>Generating explanation...</p>}
+        <div style ={{ display: "flex", gap: "20px", marginBottom: "20px"}}>
+            <div style={{ border: "1px solid white", padding: "10px", width: "300px" }}>
+                <h2>#{rank} - {listing.name}</h2>
+                {listing.matchScore !== undefined && (
+                    <p>Match Score: {listing.matchScore}</p>
+                )}
+                <p>Price: ${listing.price}</p>
+                <p>Location: {listing.location}</p>
+                <p>Commute Time: {listing.commuteTime} minutes</p>
+                <p>Lease Term: {listing.leaseTerm} months</p>
+                <p>Number of Bedrooms: {listing.numBedroom}</p>
+                <p>{listing.furnished && "Furnished"}</p>
+                <p>{listing.laundry && "Laundry"}</p>
+                <p>{listing.parking && "Parking"}</p>
+                <button onClick={() => onExplainMatch(listing)}>
+                    {explanation ? "Close Explanation" : "Review Listing"}
+                </button>
+                {isLoading && <p>Generating explanation...</p>}
+            </div>
             {explanation && (
-                <pre style={{ whiteSpace: "pre-wrap" }}>
-                {explanation}</pre>
+                <div style={{ 
+                    border: "1px solid white",
+                    padding: "10px",
+                    width: "400px"
+                }}>
+                   <pre style ={{ whiteSpace: "pre-wrap" }}>
+                    {explanation}
+                    </pre>
+                </div>
             )}
-
         </div>
     );
 } 
